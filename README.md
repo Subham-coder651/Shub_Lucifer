98257d032cee6c298d29c65239b865d321a28717
Shubham <71563770+Subham-coder651@users.noreply.github.com>
2024-11-02 09:26:35 +0545

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Slot-bot-v2
+With Multiple Category Support 
+
+
+# Features
+
+Log Channels 
+
+Ping Checker
+
+
+
+![image](https://github.com/mafiayt69/Slot-bot-v2/assets/91059034/a152ea18-1740-4918-8318-9c44ad7af4b8)
+
+![image](https://github.com/mafiayt69/Slot-bot-v2/assets/91059034/beb751f3-ad43-4d7c-80ac-44e9f6442998)
---
+++ index.js
@@ -0,0 +1,119 @@
+const {
+  Client,
+  GatewayIntentBits,
+  Collection,
+  PermissionsBitField
+} = require('discord.js');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ]
+});
+const fs = require('fs');
+
+const mongoose = require('mongoose');
+const ch = require("./other/checker")
+
+const end = require('./other/end')
+
+const cf = require('./other/config')
+
+
+//guild config moved in ""./other/config.js" there you can set
+
+
+if (!cf.config.guild_x || !cf.config.role_x || !cf.config.staff_x  || !cf.config.slotrule) {
+  
+  console.log('you need to setup config file first')
+
+  console.log(`./other/config.js Here You go for setup`)
+  return
+
+
+}
+
+
+
+
+
+
+
+client.commands = new Collection();
+client.aliases = new Collection()
+mongoose.set("strictQuery", false);
+mongoose.connect("Mongo Url").catch((e)=> {
+  console.log(e)
+  process.exit()
+
+}).then(()=> {
+  console.log('Connected To Mongo')
+});
+
+
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+let date = new Date()
+
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  console.log(`\x1b[36m%s\x1b[0m`,`[${date.toLocaleString()}] ${command.name}`)
+
+
+
+  client.commands.set(command.name, command);
+}
+
+
+client.on("ready", async () => {
+
+
+
+
+
+  console.log(`\x1b[31m%s\x1b[0m`,`${client.user.username} Is Ready`)
+})
+
+
+
+
+
+client.on("messageCreate", async message => {
+  ch(message)
+
+  if (!message.content.startsWith("-") || message.author.bot) return;
+
+
+
+
+  const args = message.content.slice("-".length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+
+
+  const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+  if (!command) return;
+
+
+
+  try {
+    command.execute(client, message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('There was an error trying to execute that command!');
+  }
+
+
+
+})
+
+
+end(client,cf)
+
+
+client.login("Your Bot Token")
