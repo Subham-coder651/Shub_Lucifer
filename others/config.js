3719ed1b5913d1211301aa5960ec8bf6ff3273e2
Shubham <71563770+Subham-coder651@users.noreply.github.com>
2024-11-02 09:35:46 +0545

    Add files via upload

---
+++ others/config.js
@@ -0,0 +1,17 @@
+const cf = {
+  config: {
+
+
+    guild_x: "None", //your guild id
+
+    role_x: "None", //slot user role id, you need this role in your profile to use slot bot 
+
+    staff_x: "None", // staff role id
+
+    logchannel: '', //channel id for slot revoke notice
+
+    slotrule: `set your slot rules`, // your slot rules
+  }
+}
+
+module.exports = cf;
